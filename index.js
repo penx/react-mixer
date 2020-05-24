9f919a2323005dfebedbf4808e9a501d9e52afa2
Alasdair McLeay <alasdair.mcleay@me.com>
2020-05-24 23:49:31 +0100

    initial publish under namespace

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Mixer } from "./components/Mixer";
+import {
+  TrackPanel,
+  TrackPanelTop,
+  TrackPanelMiddle,
+  TrackPanelBottom,
+} from "./components/TrackPanel";
+import { VolumeSlider } from "./components/VolumeSlider";
+
+export {
+  Mixer,
+  TrackPanel,
+  TrackPanelTop,
+  TrackPanelMiddle,
+  TrackPanelBottom,
+  VolumeSlider,
+};
