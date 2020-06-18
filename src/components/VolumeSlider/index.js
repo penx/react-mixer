0df7536132f4b1f13a915a28102ae76314898b8e
Alasdair McLeay <alasdair.mcleay@me.com>
2020-06-18 23:19:48 +0100

    volumeslider styles, package scripts

--- src/components/VolumeSlider/index.js
+++ src/components/VolumeSlider/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { View, TouchableOpacity } from "react-native";
 
@@ -15,19 +15,26 @@ export const VolumeSlider = ({ onChange, value, min = -128, max = 0 }) => (
     }}
   >
     <TouchableOpacity
-      style={{ height: "100%", zIndex: 1, outline: "1px solid black" }}
-      onPress={(e) =>
-        onChange(
+      style={{
+        height: HEIGHT,
+        zIndex: 1,
+        borderWidth: 1,
+        borderColor: "#000",
+        borderRadius: 6,
+      }}
+      onPress={(e) => {
+        return onChange(
           ((HEIGHT - e.nativeEvent.locationY) / HEIGHT) * (max - min) + min
-        )
-      }
+        );
+      }}
     />
     <View
       style={{
         width: 40,
-        backgroundColor: "#22A",
+        backgroundColor: "#F2A",
         touchEvents: "none",
         position: "absolute",
+        left: 10,
         bottom: 0,
         height: HEIGHT * ((value - min) / (max - min)),
       }}
